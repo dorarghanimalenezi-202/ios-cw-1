7679e48679c494c18687f907c3c6ec2ebe0eaadb
dorarghanimalenezi202 <dorar.alenezi.2@gmail.com>
2020-06-13 22:13:28 +0300

    done cw1
    done

---
+++ cw-1-dorar.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+var str = "Hello, playground"
+var name = "Dorar"
+var hobby1="horse ridding"
+var hobby2="painting"
+var age = 15
+var height = 164
+print ("name, age, height, hobby1,hobby2,")
+//
+var friendname="danah"
+var friendhobby1="reading"
+var friendhobby2="swimming"
+var friendage = 15
+var friendheight = 162
+print(friendname,friendhobby1,friendhobby2,friendage,friendheight)
+//comparing
+name == friendname
+name != friendname
+
+
+hobby1 == friendhobby2
+hobby1 != friendhobby2
+hobby2 == friendhobby2
+hobby2 != friendhobby2
+hobby1 == friendhobby2
+hobby2 != friendhobby1
+
+age > friendage
+age < friendage
+age >= friendage
+age <= friendage
+age == friendage
+age != friendage
+
+height = friendheight
+height < friendheight
+height > friendheight
+height >= friendheight
+height <= friendheight
+height != friendheight
+
+
+
+
